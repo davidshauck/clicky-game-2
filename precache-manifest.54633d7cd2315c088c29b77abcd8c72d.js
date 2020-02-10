47ee79ae5953212664db8b7f67e2aba94b55c8d8
David Hauck <dhauck@mac.com>
2020-02-09 19:54:22 -0500

    Updates

---
+++ precache-manifest.54633d7cd2315c088c29b77abcd8c72d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35a7851dc30bdf8637fff98f9134bfd8",
+    "url": "/clicky-game-2/index.html"
+  },
+  {
+    "revision": "b68e293d27a3350a3cb0",
+    "url": "/clicky-game-2/static/css/main.22f876d6.chunk.css"
+  },
+  {
+    "revision": "d3fcbafe9b1691faf591",
+    "url": "/clicky-game-2/static/js/2.432b4bc7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/clicky-game-2/static/js/2.432b4bc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b68e293d27a3350a3cb0",
+    "url": "/clicky-game-2/static/js/main.e3b586d5.chunk.js"
+  },
+  {
+    "revision": "f73953486808627b2cfb",
+    "url": "/clicky-game-2/static/js/runtime-main.b1381112.js"
+  }
+]);
